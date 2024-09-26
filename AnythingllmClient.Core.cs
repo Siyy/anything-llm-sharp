a6643c2afa4227f1de73213dc83fb47a0e22d1e4
久永 <Jiuyong@hotmail.com>
2024-09-26 16:17:38 +0800

    将 AnythingllmClient 的基本实现移入。
    分成两个部分：
    Core 实现功能无关的基础通讯。
    其它部分实现 AnythingLLM 的 API 方法。

---
+++ AnythingllmClient.Core.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuyongTests.AnythingLLM
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Net.Http;
+	using System.Text.Json.Serialization;
+	using System.Text.Json;
+	using System.IO;
+
+	public enum HttpMethod
+	{
+		Auto,
+		Get,
+		Post,
+		Delete,
+	}
+
+	public partial class AnythingllmClient
+	{
+		private readonly string _apiKey;
+		//private readonly string _secretKey;
+		readonly string _llmBaseUri;
+
+		public AnythingllmClient(string apiKey/*, string secretKey*/, string llmBaseUri = "http://localhost:3001/api")
+		{
+			_apiKey = apiKey;
+			this._llmBaseUri = llmBaseUri;
+			//_secretKey = secretKey;
+		}
+
+		public async Task<P> DoRequestAsync<Q, P>(string commandUri, P anserSample, Q message = default!, HttpMethod method = HttpMethod.Auto)
+		{
+			string url = Path.Combine(_llmBaseUri, commandUri);
+			using HttpClient client = new();
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue($"Bearer", _apiKey);
+			//var hds = client.DefaultRequestHeaders;
+			//hds.Clear();
+
+			//var timestamp = (DateTimeOffset.Now.UtcTicks - TimeZero).ToString();
+			//var timestamp = DateTime.Now.GetUtcTimeTicks().ToString();
+			string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(message);
+			//string signature = Helper.CalculateMd5(_secretKey + jsonData + timestamp);
+
+			//Dictionary<string, string> headers = new()
+			//{
+			//	//{ "Content-Type", "application/json" },
+			//	//{ "Authorization", $"Bearer {_apiKey}" },
+			//	//{ "X-BC-Request-Id", Guid.NewGuid().ToString() },
+			//	//{ "X-BC-Timestamp", timestamp },
+			//	//{ "X-BC-Signature", signature },
+			//	//{ "X-BC-Sign-Algo", "MD5" }
+			//};
+
+			StringContent content = new(jsonData, Encoding.UTF8, "application/json");
+			//var hds = content.Headers;
+			//foreach (var header in headers)
+			//{
+			//	hds.Add(header.Key, header.Value);
+			//}
+
+			HttpResponseMessage response;
+			switch (method)
+			{
+				case HttpMethod.Get:
+					response = await client.GetAsync(url);
+					break;
+				case HttpMethod.Post:
+					response = await client.PostAsync(url, content);
+					break;
+				case HttpMethod.Delete:
+					response = await client.DeleteAsync(url);
+					break;
+				case HttpMethod.Auto:
+				//break;
+				default:
+					if (null == message)
+					{
+						response = await client.GetAsync(url);
+					}
+					else
+					{
+						response = await client.PostAsync(url, content);
+					}
+					break;
+			}
+
+			if (response.IsSuccessStatusCode)
+			{
+				var p_txt = await response.Content.ReadAsStringAsync();
+				P? p;
+				p = Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(p_txt, anserSample);
+				return p ?? throw new Exception(p_txt);
+			}
+			else
+			{
+				throw new Exception($"请求失败，状态码: {response.StatusCode}");
+			}
+		}
+
+	}
+
+}
---
+++ AnythingllmClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuyongTests.AnythingLLM
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Net.Http;
+	using System.Text.Json.Serialization;
+	using System.Text.Json;
+	using System.IO;
+
+	public partial class AnythingllmClient
+	{
+
+		// /v1/workspace/{slug}/thread/{threadSlug}/chat
+		public async Task<string> DoChatAsync(string slug, string message, string threadSlug = null!)
+		{
+			var p = new
+			{
+				id = "chat-uuid",
+				type = "abort | textResponse",
+				textResponse = "Response to your query",
+				sources = new[]
+				{
+					new
+					{
+						title = "anythingllm.txt",
+						chunk = "This is a context chunk used in the answer of the prompt by the LLM,"
+					}
+				},
+				close = true,
+				error = "null | text string of the failure mode."
+			};
+
+			var uri = String.IsNullOrWhiteSpace(threadSlug) ? $"v1/workspace/{slug}/chat" : $"v1/workspace/{slug}/thread/{threadSlug}/chat";
+			var msg = new
+			{
+				message,
+				mode = "chat",
+				//userId = 1
+			};
+			var rp = await DoRequestAsync(uri, p, msg);
+			var ro = rp.textResponse;
+			return ro;
+		}
+
+	}
+
+}
